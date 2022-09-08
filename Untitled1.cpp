51dfdd932ea60e89857a0aed3f6a1161b0ff772f
afinazahrach <112566863+afinazahrach@users.noreply.github.com>
2022-09-08 09:41:19 +0700

    dev c++

---
+++ Untitled1.cpp
@@ -0,0 +1,15 @@
+  #include <iostream>
+  using namespace std;
+  int main(){
+  int p,l,luas,keliling;
+  cout<<" panjang : ";
+  cin>>p;
+  cout<<" lebar : ";
+  cin>>l;
+  luas=p*l;
+  cout<<" luas persegi : "<<luas;
+  cout<<endl;
+  keliling=p*l*p*l;
+  cout<<" keliling persegi : "<<keliling;
+  return 0;
+}
